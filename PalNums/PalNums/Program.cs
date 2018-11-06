2da0042f786739343ce9f3dcfdc6bd3bd5ff2068
John Guggenbiller <42472160+johnguggy@users.noreply.github.com>
2018-11-06 09:34:40 -0500

    Add files via upload

---
+++ PalNums/PalNums/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int num = 1;
+                while (num > 0)
+                {
+                    string counter = num.ToString();
+                    bool Pal = true;
+                    for (int i = 0; i < counter.Length / 2; i++)
+                    {
+                        if (counter[i] != counter[(counter.Length - 1) - i])
+                        {
+                            Pal = false;
+                            break;
+                        }
+                    }
+                    Console.WriteLine("The Palindrome Numbers are : ");
+                    if (Pal)
+                    {
+                        Console.WriteLine("Palindrome :" + num);
+                    }
+                    num++;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception Caught ");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
